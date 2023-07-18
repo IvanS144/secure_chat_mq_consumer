da4bf4bdfb8e923edcd886d5220e157299c8fd4f
Ivan <ivan.samardzic41@gmail.com>
2023-07-18 10:48:20 +0200

    Initial commit

---
+++ src/main/java/com/sni/secure_chat_mq_consumer/SecureChatMqConsumerApplication.java
@@ -0,0 +1,13 @@
+package com.sni.secure_chat_mq_consumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecureChatMqConsumerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SecureChatMqConsumerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sni/secure_chat_mq_consumer/SecureChatMqConsumerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sni.secure_chat_mq_consumer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecureChatMqConsumerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
